8e1cbebfe0383657e9f0ae52f9f0d20328f6054c
Арсен <arsendag505@gmail.com>
2023-11-07 20:38:20 +0300

    ;

--- front-end/scripts/index.js
+++ front-end/scripts/index.js
@@ -2,7 +2,8 @@ const load = () => {
     const messageContainer = document.querySelector('.messages')
     const input = document.querySelector('.form input')
     const form = document.querySelector('.form')
-    const socket = io();
+    const { io } = require("socket.io-client");
+    const socket = io()
     const name = atob(document.cookie.split('=')[1].split('.')[1])
 
     socket.on('add mess', data => {
