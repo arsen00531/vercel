c4ec785d8b5a1b48db4397e28c084218b2b63ffa
Арсен <arsendag505@gmail.com>
2023-11-07 23:41:27 +0300

    Update db.js

--- back-end/db/db.js
+++ back-end/db/db.js
@@ -1,7 +1,10 @@
 const Pool = require('pg').Pool
 
 const pool = new Pool({
-    connectionString: process.env.DB_URL
+    connectionString: process.env.DB_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 })
 
 module.exports = pool
