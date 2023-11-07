3358d74f34259357c38973082539ba08a929ba22
Арсен <arsendag505@gmail.com>
2023-11-07 23:00:14 +0300

    Update db.js

--- back-end/db/db.js
+++ back-end/db/db.js
@@ -1,7 +1,11 @@
 const Pool = require('pg').Pool
 
 const pool = new Pool({
-    connectionString: "postgres://arsen00531:qgk5ilD8CB812OUwyNEvM10zIHRQs2N8@dpg-cl598ok72pts739ut3ag-a/chat_hw0r",
+    host: "dpg-cl598ok72pts739ut3ag-a",
+    port: 5432,
+    database: "chat_hw0r",
+    user: "arsen00531",
+    password: "qgk5ilD8CB812OUwyNEvM10zIHRQs2N8"
 })
 
 module.exports = pool
