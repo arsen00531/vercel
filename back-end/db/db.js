09c0ed321f406dfbe92f59dc105c4c605641e710
Арсен <arsendag505@gmail.com>
2023-11-07 22:53:43 +0300

    Update db.js

--- back-end/db/db.js
+++ back-end/db/db.js
@@ -1,7 +1,7 @@
 const Pool = require('pg').Pool
 
 const pool = new Pool({
-    connectionString: process.env.PG_URL + "?sslmode=require",
+    connectionString: "postgres://arsen00531:qgk5ilD8CB812OUwyNEvM10zIHRQs2N8@dpg-cl598ok72pts739ut3ag-a/chat_hw0r",
 })
 
 module.exports = pool
