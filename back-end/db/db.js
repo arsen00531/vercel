2b248c8d6c7415e7f086da6ba9f86b26734c6992
Арсен <arsendag505@gmail.com>
2023-11-07 18:16:01 +0300

    Update db.js

--- back-end/db/db.js
+++ back-end/db/db.js
@@ -1,11 +1,11 @@
 const Pool = require('pg').Pool
 
 const pool = new Pool({
-    user: process.env.PG_USER || process.env.USER,
-    password: process.env.PG_PASSWORD || process.env.PASSWORD,
-    host: process.env.PG_HOST || process.env.HOST,
+    user: process.env.local.PG_USER || process.env.USER,
+    password: process.env.local.PG_PASSWORD || process.env.PASSWORD,
+    host: process.env.local.PG_HOST || process.env.HOST,
     port: 5432,
-    database: process.env.PG_DATABASE || process.env.DB
+    database: process.env.local.PG_DATABASE || process.env.DB
 })
 
 module.exports = pool
