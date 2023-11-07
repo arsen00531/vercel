cb1563aca578ee62e784104c01685c55e931ba50
Арсен <arsendag505@gmail.com>
2023-11-07 23:13:37 +0300

    Update db.js

--- back-end/db/db.js
+++ back-end/db/db.js
@@ -1,11 +1,7 @@
 const Pool = require('pg').Pool
 
 const pool = new Pool({
-    host: "dpg-cl598ok72pts739ut3ag-a",
-    port: 5432,
-    database: "chat_hw0r",
-    user: "arsen00531",
-    password: "qgk5ilD8CB812OUwyNEvM10zIHRQs2N8"
+    connectionString: process.env.DB_URL
 })
 
 module.exports = pool
